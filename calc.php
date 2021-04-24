740ec0efe0858312bcbd936f4f7faa7bb9abc390
Shohan20lac <63243156+Shohan20lac@users.noreply.github.com>
2021-04-24 16:41:15 +0600

    Add files via upload
    dev suspend: version 1 complete

--- calc.php
+++ calc.php
@@ -12,6 +12,7 @@
         <input type="text" name="num2" placeholder="number2">
 
         <select name="operator" id="">
+            <option value="None">None</option>
             <option value="Add">Add</option>
             <option value="Subtract">Subtract</option>
             <option value="Multiply">Multiply</option>
@@ -27,6 +28,48 @@
 
 <?php
 
+    //math functions
+    function add ($num1 , $num2){
+        return $num1+$num2;
+    }
+    function subtract ($num1 , $num2){
+        return $num1-$num2;
+    }
+    function multiply ($num1 , $num2){
+        return $num1*$num2;
+    }
+    function divide ($num1 , $num2){
+        return $num1/$num2;
+    }
+
+
+    //getting data from browser
+    if ( isset($_GET['submit']) ){
+        $num1 = $_GET['num1'];
+        $num2 = $_GET['num2'];
+        $operator = $_GET['operator'];
+
+        //deciding on which function to execute
+        switch($operator){
+            case None:
+                    echo "You need to select an operator";
+                    break;
+            case Add:
+                    echo add($num1,$num2);
+                    break;
+            case Subtract:
+                    echo subtract($num1,$num2);
+                    break; 
+            case Multiply:
+                    echo multiply($num1,$num2);
+                    break;
+            case Divide:
+                echo divide($num1,$num2);
+                break;                
+        }
+
+
+    }
 
 
 ?>
