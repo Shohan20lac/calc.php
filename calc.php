526e6f4449245835fb7ebdaa68472cabafbe41b8
Shohan20lac <63243156+Shohan20lac@users.noreply.github.com>
2021-04-24 16:03:57 +0600

    Add files via upload
    dev start

---
+++ calc.php
@@ -0,0 +1,35 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="num1" placeholder="number1">
+        <input type="text" name="num2" placeholder="number2">
+
+        <select name="operator" id="">
+            <option value="Add">Add</option>
+            <option value="Subtract">Subtract</option>
+            <option value="Multiply">Multiply</option>
+            <option value="Divide">Divide</option>
+            <option value="isPositive">isPositive</option>
+            <option value="isPositive">isPositive</option>
+        </select>
+        <br>
+        <button type="submit" name="submit" value="submit">Calculate</button>
+
+    </form>
+
+
+<?php
+
+
+
+?>
+
+</body>
+</html>
